28685802c7d1ac53dab0357a71d2635c2c7e8423
mikromann <polatt_2000@hotmail.com>
2022-01-05 23:30:17 +0300

    added more hello feature

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Developing hello feature");
+        System.out.println("doing more work");
     }
 }
